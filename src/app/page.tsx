067123eeab82e6a4797c3b5a8e4d50ba554905b5
Cindy Long <cindycs212@gmail.com>
2023-11-12 20:36:07 -0500

    comment out example api calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
+// import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-  console.log(`HELLO RESPONSE: ${JSON.stringify(hello)}`);
+  // const hello = await api.post.hello.query({ text: "from tRPC" });
+  // console.log(`HELLO RESPONSE: ${JSON.stringify(hello)}`);
   const session = await getServerAuthSession();
 
   return (
@@ -41,7 +41,7 @@ export default async function Home() {
         </div>
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
+            {/* {hello ? hello.greeting : "Loading tRPC query..."} */}
           </p>
 
           <div className="flex flex-col items-center justify-center gap-4">
@@ -57,27 +57,27 @@ export default async function Home() {
           </div>
         </div>
 
-        <CrudShowcase />
+        {/* <CrudShowcase /> */}
       </div>
     </main>
   );
 }
 
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
+// async function CrudShowcase() {
+//   const session = await getServerAuthSession();
+//   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest.query();
+//   // const latestPost = await api.post.getLatest.query();
 
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
+//   return (
+//     <div className="w-full max-w-xs">
+//       {latestPost ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no posts yet.</p>
+//       )}
 
-      <CreatePost />
-    </div>
-  );
-}
+//       {/* <CreatePost /> */}
+//     </div>
+//   );
+// }
